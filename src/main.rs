f75724cd95dadc34470757d3c865c49ef1582177
Jessy <github@jessym.com>
2024-07-26 06:55:09 +0200

    commit

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ use std::net::Ipv4Addr;
 
 use etherparse::{Ipv4HeaderSlice, TcpHeaderSlice};
 use tun_tap::{Iface, Mode};
+
 use crate::tcp::State;
 
-const PROTO_IPV4: u16 = 0x0800;
-// const PROTO_IPV6: u16 = 0x86dd;
-// const PROTO_ARP: u16 = 0x0806;
+// Proto field in Ethernet frame header
+const ETHERNET_FRAME_PROTO_IPV4: u16 = 0x0800;
+
+// Proto field in the IP packet header
+const IP_PACKET_PROTO_TCP: u8 = 0x06;
 
 #[derive(Hash, PartialEq, Eq)]
 struct Socket {
@@ -27,6 +30,7 @@ struct SocketPair {
 
 fn main() -> io::Result<()> {
     let mut connections = HashMap::<SocketPair, State>::new();
+
     /*
     The TUN mode means: you'll be supplied with raw IP packets
     The TAP mode means: you'll be supplied with raw Ethernet frames
@@ -35,6 +39,7 @@ fn main() -> io::Result<()> {
     let mut buffer = [0u8; 1504];
     loop {
         let incoming_frame_total_size = iface.recv(&mut buffer[..])?;
+
         /*
         By default, tun/tap prepends packet information (4 bytes) for incoming data:
          - 2 bytes for flags
@@ -46,38 +51,46 @@ fn main() -> io::Result<()> {
         let incoming_frame_tuntap_prepended_info_length = 4;
         let incoming_frame_tuntap_flags = u16::from_be_bytes([buffer[0], buffer[1]]);
         let incoming_frame_tuntap_proto = u16::from_be_bytes([buffer[2], buffer[3]]);
-
-        let ip_packet = &buffer[incoming_frame_tuntap_prepended_info_length..incoming_frame_total_size];
-
-        /*
-        For the `incoming_frame_tuntap_proto`, you might get the value 0x86DD
-         - this refers to the protocol number of the parent Ethernet frame
-         - in case of 0x86DD, it's an IPv6 packet: https://en.wikipedia.org/wiki/EtherType
-        For convenience, let's ignore anything which isn't IPv4 on the next line
-
-        Quick side note, we would have only gotten IP packets (ipv4 or ipv6) at this point anyway...
-        ...because our interface operates in TUN mode, which filters out ARP packets and anything part of an ENET frame which doesn't declare its payload to be an IP packet
-         */
-        if incoming_frame_tuntap_proto != PROTO_IPV4 {
+        if incoming_frame_tuntap_proto != ETHERNET_FRAME_PROTO_IPV4 {
+            /*
+            You might get some frames with EtherType 0x86DD here...
+             - this refers to the protocol number of the parent Ethernet frame
+             - in case of 0x86DD, it's an IPv6 packet: https://en.wikipedia.org/wiki/EtherType
+            For convenience, we ignore anything which isn't IPv4
+             */
+            eprintln!("Skipping unsupported Ethernet frame: 0x{:x}", incoming_frame_tuntap_proto);
             continue;
+            /*
+            Quick side note, we would have only gotten IP packets (ipv4 or ipv6) at this point anyway...
+            ...because our interface operates in TUN mode, which filters out ARP packets and any other types of Ethernet frames payloads
+             */
         }
+
+        // We know that the payload "must" (should) be an IP packet at this point
+        let ip_packet = &buffer[incoming_frame_tuntap_prepended_info_length..incoming_frame_total_size];
         match Ipv4HeaderSlice::from_slice(ip_packet) {
             Err(e) => {
-                eprintln!("Skipping weird IPv4 packet: {:?}", e)
+                eprintln!("Skipping invalid IPv4 packet: {:?}", e)
             }
             Ok(ipv4_header) => {
                 let ip_packet_src_addr = ipv4_header.source_addr();
                 let ip_packet_dest_addr = ipv4_header.destination_addr();
+                let ip_packet_proto = ipv4_header.protocol();
+                if ip_packet_proto.0 != IP_PACKET_PROTO_TCP {
+                    eprintln!("Skipping unsupported IPv4 packet: {:?}", ip_packet_proto);
+                    continue;
+                }
+
+                // We know that the payload "must" (should) be a TCP segment at this point
                 let tcp_segment = &ip_packet[ipv4_header.slice().len()..];
                 match TcpHeaderSlice::from_slice(tcp_segment) {
                     Err(e) => {
-                        eprintln!("Skipping weird TCP packet: {:?}", e);
+                        eprintln!("Skipping invalid TCP packet: {:?}", e);
                     }
                     Ok(tcp_header) => {
                         let tcp_segment_src_port = tcp_header.source_port();
                         let tcp_segment_dest_port = tcp_header.destination_port();
                         let tcp_payload = &tcp_segment[tcp_header.slice().len()..];
-
                         eprintln!(
                             "🎭 [EFRAME_FLAGS={}, EFRAME_PROTO=0x{:x}, IP_PACKET_SIZE={}]",
                             incoming_frame_tuntap_flags,
